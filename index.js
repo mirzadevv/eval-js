aa41da2adc80777e5098374fcbe445fff1633da7
mirzaei.dev <mirzaei.devv@gmail.com>
2022-06-09 14:40:00 +0430

    feat: calculation of first priority (* , /)

--- index.js
+++ index.js
@@ -17,7 +17,28 @@ const myEval = (expression) => {
     }
   }
 
-  // calculation of the second priority (+ , -)
+  // calculation of first priority (* , /)
+  var firstPriorityResult = 0;
+  for (i = 0; i < arrayExpression.length; i++) {
+    let firstPriority = arrayExpression.find(
+      (item) => item === "*" || item === "/"
+    );
+    let indexOfFirstPriority = arrayExpression.indexOf(firstPriority);
+    if (firstPriority === "*") {
+      firstPriorityResult =
+        arrayExpression[indexOfFirstPriority - 1] *
+        arrayExpression[indexOfFirstPriority + 1];
+    } else if (firstPriority === "/") {
+      firstPriorityResult =
+        arrayExpression[indexOfFirstPriority - 1] /
+        arrayExpression[indexOfFirstPriority + 1];
+    } else {
+      break;
+    }
+    arrayExpression.splice(indexOfFirstPriority - 1, 3, firstPriorityResult);
+  }
+
+  // calculation of second priority (+ , -)
   var secArrayExpression = [...arrayExpression];
   var secondPriorityResult = 0;
   for (i = 0; i < secArrayExpression.length; i++) {
@@ -47,3 +68,4 @@ const myEval = (expression) => {
 
 let expression = "8*2+4/1";
 const result = myEval(expression);
+console.log("Result:", result);
